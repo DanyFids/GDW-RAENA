3305a1b01f6c765be72961cf276fa14629833123
Anthony <anthony.ona@uoit.net>
2019-03-07 13:59:57 -0500

    Levels

---
+++ Levels/Levels.cpp
@@ -0,0 +1,8 @@
+#include "Levels.h"
+
+bool Tut_level_1::init()
+{
+	if(GameplayScene::init())
+
+	return false;
+}
---
+++ Levels/Levels.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "GameplayScene.h"
+
+class Tut_level_1 : public GameplayScene{
+public:
+	virtual bool init();
+};
